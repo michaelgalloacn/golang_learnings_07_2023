d34b9193a78b277bbb428e185053eaee151122a6
Michael Gallo <michael.gallo@corecompete.com>
2023-07-27 14:27:24 -0400

    Better comments

--- collections_and_iterations/maps/maps.go
+++ collections_and_iterations/maps/maps.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// commmon implementation for enums
+// commmon implementation for enums is to declare a set of constants in the same block, often with a custom type
 
 type founder string
 
@@ -17,29 +17,33 @@ type fakeType string
 
 func main() {
 
+	// Syntax to create a map is make(map[keyType]valType)
 	foundersToCompanies := make(map[founder]string)
 
 	foundersToCompanies[Linus_Torvalds] = "Linux Foundation"
 	foundersToCompanies[Clarence_Delany] = "Accenture"
 	foundersToCompanies[Richard_Fairbank] = "Capital One"
 
+	// Golang supports multiple returns
+	// Accessing a value from a map returns two values
+	// First value is zero value for return type if key is not found
+	// Second value is bool representing whether the key is in the map
 	unknownCompany, notInMap := foundersToCompanies[Bill_Gates]
-
 	fmt.Println(unknownCompany, notInMap)
 
 	// Example of a company that IS in the map
 	knownCompany, isInMap := foundersToCompanies[Linus_Torvalds]
 	fmt.Println(knownCompany, isInMap)
 
-	// Length of a map
+	// Length of a map is found with built in len function
 	fmt.Println("Number of known companies:\n", len(foundersToCompanies))
 
-	// Delete operation for a hashmap
+	// Deletion operations use built in delete function with map as its first argument and key as its second
 	delete(foundersToCompanies, Linus_Torvalds)
 	exKnownCompany, noLongerInMap := foundersToCompanies[Linus_Torvalds]
 	fmt.Println(exKnownCompany, noLongerInMap)
 
-	// Iterating through a map
+	// Iterating through the range operator returns two values, key and value
 	for name, company := range foundersToCompanies {
 		fmt.Println(name, ":", company)
 	}
